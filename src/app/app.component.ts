38806845508513b2783dc13d3dba44492496a95e
miz <mizerablepi@gmail.com>
2024-03-05 16:15:44 +0530

    add root template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,32 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { AuthService } from '@/services/auth.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'watch_list';
+  drawerOpen = false;
+  loggedIn = false;
+
+  constructor(private auth: AuthService) {}
+
+  drawerClick() {
+    this.drawerOpen = !this.drawerOpen;
+  }
+
+  ngOnInit(): void {
+    if (this.auth.checkUser()) {
+      this.loggedIn = true;
+    }
+  }
+
+  logout() {
+    this.drawerOpen = false;
+    this.loggedIn = false;
+  }
 }
